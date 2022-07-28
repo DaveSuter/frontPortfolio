03dd766afdc0305a6b732c3924e1931bac67d043
DaveSuter <daveysuter@gmail.com>
2022-07-28 20:10:54 -0300

    prueba

--- src/app/modelos/login-usuario.ts
+++ src/app/modelos/login-usuario.ts
@@ -1,11 +1,9 @@
 export class LoginUsuario {
     userName: String;
     password: String;
-    asd: String;
 
     constructor(userName: String, password: String, asd: String){
         this.userName = userName;
         this.password = password;
-        this.asd = asd;
     }
 }
